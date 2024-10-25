01c8ea0957825a3179d1a73955633cd5632ffbd7
Mathieu Hardy <mhardy2008@gmail.com>
2024-10-25 10:24:50 +0200

    minor changes

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,10 +1,12 @@
+pub use async_trait::async_trait;
+pub use serde::{Deserialize, Serialize};
+pub use uuid::Uuid;
+
 pub use crate::error::*;
 pub use crate::job::*;
 pub use crate::job_queue::*;
 pub use crate::job_queue_builder::*;
-pub use serde::{Deserialize, Serialize};
+pub use crate::types::*;
 
 pub(crate) use crate::api_err;
 pub(crate) use crate::backend::*;
-pub(crate) use crate::types::*;
-pub(crate) use uuid::Uuid;
